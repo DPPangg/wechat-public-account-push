ba0c7aed33f9f5b1f8b6eb3d39aa8ed80d02d114
DPPangg <122730804+DPPangg@users.noreply.github.com>
2023-01-15 22:20:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oQr4E50kHcEI7vTcesZuZnTQMHcI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'lQNfGGV-se4Ysb9Cf4mn9SVwccjzXlP0fsR7oZdm5wI',
+      useTemplateId: '7GW6W46xGfymxKe76C2bf-XDuVasETwrc13iyVSc90Q',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-27',
       festivals: [
@@ -47,7 +47,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'lQNfGGV-se4Ysb9Cf4mn9SVwccjzXlP0fsR7oZdm5wI',
+  CALLBACK_TEMPLATE_ID: '	7GW6W46xGfymxKe76C2bf-XDuVasETwrc13iyVSc90Q',
 
   CALLBACK_USERS: [
     {
