cd18db57f17405580874c1f8e25f2e47fc81f536
DPPangg <122730804+DPPangg@users.noreply.github.com>
2023-01-15 21:17:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,18 +31,13 @@ const USER_CONFIG = {
         {
           type: '*生日', name: '小宝', year: '2003', date: '03-27',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '小宝', year: '2003', date: '03-27',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2021', date: '09-05',
-        },
+        
+
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2021-12-01' },
+        { keyword: 'love_day', date: '2021-12-02' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
